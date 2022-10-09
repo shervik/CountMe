fe99392455285aae8cc23d5f0cbbb82773f7a9d4
Victoria Scherbakova <gesvik5@gmail.com>
2022-10-09 21:27:21 +0300

    create a new counting app

--- CountMe/ViewController.swift
+++ CountMe/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    private var counter: Int = 0
+    
+    @IBAction func resetCount(_ sender: UIButton) {
+        counter = 0
+        countLabel.text = "\(counter)"
+    }
+    
+    @IBAction func increase(_ sender: UIButton) {
+        counter += 1
+        countLabel.text = "\(counter)"
+    }
+    
+    @IBAction func decrease(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            countLabel.text = "\(counter)"
+        } else {
+            let alert = UIAlertController(title: "Ошибка", message: "Мы не можем посчитать меньше 0 :(", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Понятно", style: .destructive, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonShadow(increaseButton)
+        buttonShadow(decreaseButton)
+        buttonShadow(resetButton)
     }
-
-
+    
+    func buttonShadow(_ button: UIButton){
+        button.layer.shadowColor = UIColor.darkGray.cgColor
+        button.layer.shadowOffset = CGSize(width: 5, height: 5)
+        button.layer.shadowRadius = 4.0
+        button.layer.shadowOpacity = 0.4
+    }
 }
 
