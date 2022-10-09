69c67c1eb13a47e8a7a9f2fd74bd38a590c587b5
Victoria Scherbakova <gesvik5@gmail.com>
2022-10-09 21:19:45 +0300

    Initial Commit

---
+++ CountMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountMe
+//
+//  Created by Виктория Щербакова on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
